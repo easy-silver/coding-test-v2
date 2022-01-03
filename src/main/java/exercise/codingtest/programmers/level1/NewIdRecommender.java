6ba26687933b8a56c762bf607fca78db7b99f276
Timo <developer.timo@gmail.com>
2022-01-03 17:52:02 +0900

    [프로그래머스 LV.1] 신규 아이디 추천 * 7단계: '길이가 2자 이하라면, 마지막 문자를 길이가 3이 될 때까지 반복해서 끝에 붙인다' 구현 및 검증

--- src/main/java/exercise/codingtest/programmers/level1/NewIdRecommender.java
+++ src/main/java/exercise/codingtest/programmers/level1/NewIdRecommender.java
@@ -40,7 +40,7 @@ public class NewIdRecommender {
         return userId.equals("") ? "a" : userId;
     }
 
-    //new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+    //6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
     //만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
     public String cutLessThan16(String userId) {
 
@@ -51,4 +51,13 @@ public class NewIdRecommender {
             return userId;
         }
     }
+
+    //7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+    public String appendUntilLength3(String userId) {
+        while (userId.length() < 3) {
+            userId = userId + userId.charAt(userId.length()-1);
+        }
+        return userId;
+    }
+
 }
--- src/test/java/exercise/codingtest/programmers/level1/NewIdRecommenderTest.java
+++ src/test/java/exercise/codingtest/programmers/level1/NewIdRecommenderTest.java
@@ -52,7 +52,7 @@ class NewIdRecommenderTest {
         assertThat(recommender.removeDotAtSide("abcdefghijklmn.p")).isEqualTo("abcdefghijklmn.p");
     }
 
-    @DisplayName("5단계 빈 문자열을 'a'로 대체한다")
+    @DisplayName("5단계: 빈 문자열을 'a'로 대체한다")
     @Test
     void step_5() {
         assertThat(recommender.changeEmptyToA("bat.y.abcdefghijklm")).isEqualTo("bat.y.abcdefghijklm");
@@ -62,7 +62,7 @@ class NewIdRecommenderTest {
         assertThat(recommender.changeEmptyToA("abcdefghijklmn.p")).isEqualTo("abcdefghijklmn.p");
     }
 
-    @DisplayName("길이가 16자 이상이면 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거한다." +
+    @DisplayName("6단계: 길이가 16자 이상이면 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거한다." +
             "만약 제거 후 마침표(.)가 끝에 위치한다면 마침표(.) 문자를 제거한다.")
     @Test
     void step_6() {
@@ -71,6 +71,16 @@ class NewIdRecommenderTest {
         assertThat(recommender.cutLessThan16("a")).isEqualTo("a");
         assertThat(recommender.cutLessThan16("123_.def")).isEqualTo("123_.def");
         assertThat(recommender.cutLessThan16("abcdefghijklmn.p")).isEqualTo("abcdefghijklmn");
+    }
+
+    @DisplayName("7단계: 길이가 2자 이하라면, 마지막 문자를 길이가 3이 될 때까지 반복해서 끝에 붙인다")
+    @Test
+    void step_7() {
+        assertThat(recommender.appendUntilLength3("bat.y.abcdefghi")).isEqualTo("bat.y.abcdefghi");
+        assertThat(recommender.appendUntilLength3("z-")).isEqualTo("z--");
+        assertThat(recommender.appendUntilLength3("a")).isEqualTo("aaa");
+        assertThat(recommender.appendUntilLength3("123_.def")).isEqualTo("123_.def");
+        assertThat(recommender.appendUntilLength3("abcdefghijklmn")).isEqualTo("abcdefghijklmn");
     }
 
 }
