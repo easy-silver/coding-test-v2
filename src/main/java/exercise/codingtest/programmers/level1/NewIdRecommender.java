5e2c8332167972e3db9a837a8a6da8b2530a0f78
Timo <developer.timo@gmail.com>
2022-01-03 23:34:46 +0900

    refactor: [프로그래머스 LV.1] 로또의 최고 순위와 최저 순위 - 정규식으로 변경

--- src/main/java/exercise/codingtest/programmers/level1/NewIdRecommender.java
+++ src/main/java/exercise/codingtest/programmers/level1/NewIdRecommender.java
@@ -8,7 +8,6 @@ import org.springframework.util.StringUtils;
  */
 public class NewIdRecommender {
 
-
     //1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
     public String toLowerCase(String userId) {
         return userId.toLowerCase();
@@ -26,13 +25,7 @@ public class NewIdRecommender {
 
     //4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
     public String removeDotAtSide(String userId) {
-        if (userId.charAt(0) == '.') {
-            userId = userId.substring(1);
-        }
-        if (StringUtils.hasText(userId) && userId.charAt(userId.length() - 1) == '.') {
-            userId = userId.substring(0, userId.length() - 1);
-        }
-        return userId;
+        return userId.replaceAll("^[.]|[.]$", "");
     }
 
     //5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
@@ -43,19 +36,13 @@ public class NewIdRecommender {
     //6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
     //만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
     public String cutLessThan16(String userId) {
-
-        if (userId.length() > 15) {
-            userId = userId.substring(0, 15);
-            return userId.charAt(userId.length() - 1) == '.' ? userId.substring(0, userId.length() -1) : userId;
-        } else {
-            return userId;
-        }
+        return userId.length() > 15 ? userId.substring(0, 15).replaceAll("^[.]|[.]$", "") : userId;
     }
 
     //7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
     public String appendUntilLength3(String userId) {
         while (userId.length() < 3) {
-            userId = userId + userId.charAt(userId.length()-1);
+            userId += userId.charAt(userId.length()-1);
         }
         return userId;
     }
