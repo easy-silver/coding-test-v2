6c7ea766f514d349fc319674453d63221a676128
Timo <developer.timo@gmail.com>
2022-02-23 07:58:45 +0900

    Refactor: [LeetCode - easy] TwoSum
    - 배열에 매번 접근하지 않도록 변수에 값을 담아 계산

--- src/main/java/exercise/codingtest/leetcode/easy/TwoSum.java
+++ src/main/java/exercise/codingtest/leetcode/easy/TwoSum.java
@@ -7,24 +7,19 @@ public class TwoSum {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        int firstIndex, secondIndex = -1;
-
         //1. nums의 길이만큼 반복문을 실행한다.
         for (int i = 0; i < nums.length; i++) {
-            //2. 첫 인덱스 번호를 변수에 담는다.
-            firstIndex = i;
+            int remainNumber = target - nums[i];
+
             //3. 첫 인덱스 +1의 인덱스 값부터 배열의 크기만큼 반복하는 반복문을 실행한다.
             for (int j = i + 1; j < nums.length; j++) {
                 //4. 첫 인덱스 값과 더한 값이 target 값이 되는 수를 찾는다.
-                if (nums[i] + nums[j] == target) {
-                    //5-1. 찾았다면 두 번째 인덱스를 변수에 담는다.
-                    secondIndex = j;
-
-                    return new int[]{firstIndex, secondIndex};
+                if (nums[j] == remainNumber) {
+                    return new int[]{i, j};
                 }
                 //5-2. 못 찾았다면 두 번째 인덱스부터 1번의 반복문을 다시 실행한다.
             }
         }
-        return null;
+        return new int[2];
     }
 }
