d7dc6ba936257bf900a78e9b618142abb57d50c4
Timo <developer.timo@gmail.com>
2022-02-23 10:07:11 +0900

    Fix: [LeetCode] #217 Contains Duplicate
    - 정렬을 사용해 반복문을 하나로 줄여 수행 속도 개선

--- src/main/java/exercise/codingtest/leetcode/easy/ContainsDuplicate.java
+++ src/main/java/exercise/codingtest/leetcode/easy/ContainsDuplicate.java
@@ -1,25 +1,24 @@
 package exercise.codingtest.leetcode.easy;
 
+import java.util.Arrays;
+
 /**
  * Easy - #217
  */
 public class ContainsDuplicate {
 
     public boolean containsDuplicate(int[] nums) {
+        //배열을 오름차 순으로 정렬한다.
+        Arrays.sort(nums);
 
         //첫 번째 인덱스부터 nums 길이-1만큼 반복한다.
         for (int i = 0; i < nums.length - 1; i++) {
             //현재 인덱스의 값을 변수에 담는다.
             int targetValue = nums[i];
-
-            //현재 인덱스의 다음 인덱스부터 nums의 길이만큼 반복한다.
-            for (int j = i+1; j < nums.length; j++) {
-                //첫 번째 인덱스 값과 현재 인덱스의 값이 같은지 확인한다.
-                if (targetValue == nums[j]) {
-                    //같다면 true를 리턴한다.
-                    return true;
-                }
+            if (targetValue == nums[i + 1]) {
+                return true;
             }
+
         }
         return false;
     }
