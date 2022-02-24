2622110ed1db63de69de0afb73d427fe66f8a327
Timo <developer.timo@gmail.com>
2022-02-25 08:00:58 +0900

    Refactor: [LeetCode] #268 Missing Number
    - 배열 합의 차를 이용해서 속도 개선

--- src/main/java/exercise/codingtest/leetcode/easy/MissingNumber.java
+++ src/main/java/exercise/codingtest/leetcode/easy/MissingNumber.java
@@ -1,30 +1,19 @@
 package exercise.codingtest.leetcode.easy;
 
-import java.util.Arrays;
-
 /**
  * 268. Missing Number
  */
 public class MissingNumber {
 
     public int missingNumber(int[] nums) {
-        //배열을 오름차순으로 정렬한다.
-        Arrays.sort(nums);
+        int sumOfRange = nums.length;
+        int sumOfNums = 0;
 
-        if (nums[0] != 0) {
-            return 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumOfRange += i;
+            sumOfNums += nums[i];
         }
 
-        //배열의 길이-1만큼 반복하며 i번째 값에 1을 더했을 때 i+1의 차가 1이 아닌 경우를 찾는다.
-        for (int i = 0; i < nums.length - 1; i++) {
-
-            int currentValue = nums[i];
-            int nextValue = nums[i + 1];
-            if (currentValue + 1 != nextValue) {
-                return currentValue + 1;
-            }
-        }
-
-        return nums.length;
+        return sumOfRange - sumOfNums;
     }
 }
