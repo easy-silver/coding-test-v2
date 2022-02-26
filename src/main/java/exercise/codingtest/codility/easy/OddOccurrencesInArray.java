6107d9c98c8456154c420646428078061b801a0b
Timo <developer.timo@gmail.com>
2022-02-26 12:36:19 +0900

    Refactor: [Codility] 2. Odd Occurrences In Array
    - Set을 사용하여 중복 제거
    - Score 88%

--- src/main/java/exercise/codingtest/codility/easy/OddOccurrencesInArray.java
+++ src/main/java/exercise/codingtest/codility/easy/OddOccurrencesInArray.java
@@ -1,44 +1,22 @@
 package exercise.codingtest.codility.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class OddOccurrencesInArray {
 
     public int solution(int[] A) {
+        Set<Integer> set = new HashSet<>();
 
-        //A의 길이 -1 만큼 반복
-        int lengthOfArray = A.length;
-        for (int i = 0; i < lengthOfArray - 1; i++) {
-            int firstNumber = A[i];
-
-            //현재 인덱스 값이 0이 아닌지 확인
-            if (firstNumber == 0) {
-                continue;
-            }
-
-            //현재 인덱스 + 1부터 시작해서 A의 길이만큼 반복
-            for (int j = i + 1; j < lengthOfArray; j++) {
-                int secondNumber = A[j];
-
-                //현재 인덱스 값이 0이 아닌지 확인
-                if (secondNumber == 0) {
-                    continue;
-                }
-
-                //현재 인덱스 값과 +j의 값이 같은지 확인
-                if (firstNumber == secondNumber) {
-                    //같다면 i번째 값과 j번째 값을 0 으로 치환
-                    A[i] = 0;
-                    A[j] = 0;
-                    break;
-                }
-            }
-        }
-
-        //배열 길이만큼 반복하며 0이 아닌 수 반환
         for (int number : A) {
-            if (number != 0) {
-                return number;
+            if (set.contains(number)) {
+                set.remove(number);
+
+            } else {
+                set.add(number);
             }
         }
-        return 0;
+
+        return set.stream().findAny().get();
     }
 }
